9e7759657ce2c553ce44420407993867e8690f5b
govind singh <87171832+govind7773@users.noreply.github.com>
2023-05-04 22:14:44 +0530

    cdbs

--- getloc.js
+++ getloc.js
@@ -1,15 +1,2 @@
-$(document).ready(function (){
-
- function showPosition(position) {
-        console.log( position.coords.latitude , position.coords.latitude );
-                   $( "#addout" ).text( position.coords.latitude );
 
 
-      }
-    $('#getloc').click(function(){   
-         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-          }
-    });
-  
-});
